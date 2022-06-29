52e90153194c539090c1408c3c9b3e1e18691898
Joel <jcbluemel@yahoo.com>
2022-06-29 16:07:54 -0700

    added /items/:name

--- app.js
+++ app.js
@@ -5,23 +5,42 @@ const app = express();
 app.use(express.json());                           // process JSON data
 app.use(express.urlencoded({ extended: true }));   // process trad form data
 
-const {items} = require("./fakeDb");
+const { items } = require("./fakeDb");
 
 
 const { NotFoundError, BadRequestError } = require("./expressError");
 
 
+/** Returns list of items. */
 
-app.get("/items", function(req, res) {
+app.get("/items", function (req, res) {
   return res.json({ items });
 });
 
+/** Accepts JSON body, add item to list,
+ *  Return JSON: {added: { name, price}, ... } */
 
-app.post("/items", function(req, res) {
-  let newItem = { name: req.body.name, price: req.body.price};
+app.post("/items", function (req, res) {
+  let newItem = { name: req.body.name, price: req.body.price };
   items.push(newItem);
-  return res.json({added: newItem});
-})
+  return res.json({ added: newItem });
+});
+
+/** Given a name as URL param, find and return that item.
+ *  Throw 404 Error if not found.
+ */
+
+app.get("/items/:name", function (req, res) {
+
+  for (let item of items) {
+
+    if (item.name === req.params.name) {
+
+      return res.json(item);
+    }
+  }
+  throw new NotFoundError("Item does not exist.");
+});
 
 /** 404 handler: matches unmatched routes; raises NotFoundError. */
 app.use(function (req, res, next) {
